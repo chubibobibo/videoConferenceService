978b21031021c835678d042763856c12678d9d02
Alistair Abao <abaolester@gmail.com>
2024-08-19 15:29:03 +0200

    passportJS for hashing password upon registration

--- server/server.js
+++ server/server.js
@@ -6,8 +6,17 @@ import cors from "cors";
 
 import userRoutes from "./routes/userRoutes.js";
 
+/** Session */
+import MongoStore from "connect-mongo";
+import session from "express-session";
+import { UserModel } from "./models/UserSchema.js";
+
+import passport from "passport";
+// import localStrategy from "passport-local";
+
 const app = express();
 
+/** parsing of json */
 app.use(cors());
 app.use(express.json());
 
@@ -17,6 +26,57 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 }
+
+/** Configuring Session for passportJS*/
+/** User schema needs to be configured to use passport local mongoose (creation of username nad password)*/
+
+/** mongo store to prevent memory leaks */
+/** @touchAfter updates session only once in 24hrs */
+export const store = MongoStore.create({
+  mongoUrl: process.env.MONGO_URL,
+  secret: process.env.STORE_SECRET,
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("session error");
+});
+
+/** @store session created to store the session data. comes from the file sessionStore.js  */
+/** @cookie will be stored in the browser that will be used to access the session server side */
+export const sessionConfig = {
+  store,
+  name: process.env.SESSION_NAME,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+/** Instantiate the session using the session config */
+app.use(session(sessionConfig));
+
+/** middleware to initialize passportJS for incoming requests */
+app.use(passport.initialize());
+app.use(passport.session()); /** allows persistent sessions */
+
+/** test if session and user data exist in every request */
+app.use((req, res, next) => {
+  console.log(req.user);
+  console.log(req.session);
+  next();
+});
+
+/**allows passport to use the local strategy plugin in the UserModel */
+passport.use(UserModel.createStrategy());
+
+/** @serializeUser stores data in session using passport*/
+/** @deserializeUser removes data from session */
+passport.serializeUser(UserModel.serializeUser());
+passport.deserializeUser(UserModel.deserializeUser());
 
 /** Routes */
 app.use("/api/users", userRoutes);
