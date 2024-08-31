1a3476ce41ca33458cec7e01c1e7da2e437beb39
Alistair Abao <abaolester@gmail.com>
2024-09-01 00:32:34 +0200

    setting up socket client

--- client/src/pages/dashboardPages/RoomPage.jsx
+++ client/src/pages/dashboardPages/RoomPage.jsx
@@ -1,4 +1,14 @@
+import { io } from "socket.io-client";
+import { useEffect } from "react";
+
 function RoomPage() {
+  /** @WS stores the web socket url */
+  const WS = "http://localhost:3001";
+
+  /** Initialize a websocket connection upon mounting of component */
+  useEffect(() => {
+    io(WS);
+  }, []);
   return <div>RoomPage</div>;
 }
 export default RoomPage;
