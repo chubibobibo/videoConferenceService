715b101213ef88ecc09fe06a4d8199b19f3b4f7b
Alistair Abao <abaolester@gmail.com>
2024-09-14 22:19:18 +0200

    fixed adding participants using peerId

--- server/sockets/RoomHandler.js
+++ server/sockets/RoomHandler.js
@@ -12,12 +12,46 @@ export const roomHandler = (socket) => {
   socket.on("create-room", async (roomName) => {
     console.log("User created room");
     const roomId = uuidv4();
-    await RoomModel.create({ roomId: roomId, roomName: roomName });
-    socket.emit("room-created", { roomId, roomName });
+    try {
+      await RoomModel.create({
+        roomId: roomId,
+        roomName: roomName,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    socket.emit("room-created", {
+      roomId: roomId,
+      roomName: roomName,
+    });
+    return;
   });
   /** Listener for "join-room" */
-  socket.on("join-room", ({ roomId }) => {
-    console.log("User joined the room", roomId);
-    socket.join(roomId);
+  /** @peerId logged user id converted by peerjs emitted from RoomPage.jsx */
+  /** @username username of logged user emitted from RoomPage */
+  /** @foundRoom current room that is created where we are going to push participants using peerId */
+  /** @updatedRoom searching for a specific room (room created or joined) to push peerId into participants */
+  socket.on("join-room", ({ roomId, peerId, username }) => {
+    const updateParticipants = async () => {
+      const foundRoom = await RoomModel.findOne({ roomId: roomId });
+      if (foundRoom && peerId !== null) {
+        const updatedRoom = await RoomModel.findOneAndUpdate(
+          { roomId: roomId },
+          { $push: { participants: peerId && peerId } },
+          { new: true }
+        );
+        // console.log(updatedRoom);
+        console.log({ peerId, username, roomId });
+        if (peerId) {
+          console.log(`User id: ${peerId} joined the room id: ${roomId}`);
+        }
+        socket.join(roomId);
+        socket.emit("get-users", {
+          roomId,
+          participants: updatedRoom.participants,
+        });
+      }
+    };
+    updateParticipants();
   });
 };
